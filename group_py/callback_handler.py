202562145f4f7c03147fd83c375f0e6e4d5c131b
Mitch Faircloth <Mfaircloth67@gmail.com>
2023-08-14 15:58:15 -0700

    simplified the logic by making _routes a dict

--- group_py/callback_handler.py
+++ group_py/callback_handler.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 from datetime import datetime
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
@@ -66,21 +66,21 @@ class Route:
 class MessageRouter:
     '''Routes messages to appropriate handlers.'''
 
-    _routes: List[Route]
+    _routes: Dict[str, Route]
 
     def __init__(self, handlers: List[MessageHandler]) -> None:
-        self._routes = [Route(handler.__name__, handler) for handler in handlers]
+        self._routes = { handler.__name__: Route(handler.__name__, handler) for handler in handlers }
 
     @property
     def get_routes(self) -> List[Route]:
         return self._routes
 
     def get_route_by_name(self, name: str):
-        return next((route for route in self._routes if route.name == name), None)
+        return self._routes.get(name)
 
     def route(self, message: Message) -> None:
         '''Routes message to all applicable handlers.'''
-        for route in self._routes:
+        for route in self._routes.values():
             if route.handler.can_handle(message):
                 route.handler.execute(message)
                 route.executions += 1
